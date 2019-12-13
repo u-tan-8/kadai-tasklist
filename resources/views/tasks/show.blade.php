e21fcdd16a5091d9b28cbcb0eb5c0985da897f99
u-tan-8 <58318986+u-tan-8@users.noreply.github.com>
2019-12-13 16:39:12 +0900

    Update show.blade.php

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -17,7 +17,7 @@
 
     {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-light']) !!}
     
-    {!! Form::model($task, ['route' => ['task.destroy', $task->id], 'method' => 'delete']) !!}
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 
