966a628ad682017d2c20cc935a0939de45b06826
yuuji.maeda <utan.ym@gmail.com>
2019-12-27 10:19:01 +0000

    update

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -162,7 +162,7 @@ class TasksController extends Controller
     public function destroy($id)
     {
         $task = Task::find($id);
-        if (\Auth::id() === $micropost->user_id) {
+        if (\Auth::id() === $task->user_id) {
         $task->delete();
         }
         
